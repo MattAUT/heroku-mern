83a17a3ee8a520a54308fad28578000dcb80b36f
MattAUT <110206916+MattAUT@users.noreply.github.com>
2022-08-12 14:05:53 +1200

    fix it

--- backend/src/routes/books.js
+++ backend/src/routes/books.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const Book = require("../models/Book");
+const Book = require("../models/book");
 
 router.get("/test", (req, res) => res.send("book route testing!"));
 
